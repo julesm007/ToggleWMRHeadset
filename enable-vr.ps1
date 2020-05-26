aecbc424077aced7d5180b47f77f5ad63d89c076
julesm007 <julianmarks@imap-mail.com>
2020-05-26 17:34:48 -0400

    Create enable-vr.ps1

---
+++ enable-vr.ps1
@@ -0,0 +1,3 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+Enable-PnpDevice -InstanceId 'USB\VID_045E&PID_0659&MI_04\7&13c082dc&0&0004' -confirm:$false
